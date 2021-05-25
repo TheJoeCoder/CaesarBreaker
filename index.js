a29461c6a337d721ba92410b6b7b9a04fdda5965
TheJoeCoder <joe@radialbog9.uk>
2021-05-25 16:32:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+// Caesar cipher breaker
+const fs = require("fs");
+
+const list = fs.readFileSync("./words.txt").toString();
+const words = list.split("\n");
+
+const args = process.argv.slice(2);
+
+var letters1 = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+var letters2 = letters1;
+
+//loop through all words specified in the arguments
+for (var i = 0; i < args.length; i++) {
+  //loop through all cipher patterns
+  for (var j = 1; j < letters1.length; j++) {
+    //cycle cipher
+    console.log(`Using cipher ${j} (A = ${letters2[0]})`);
+    var start = letters2[0]; //store first letter
+    letters2.shift(); //remove first letter
+    letters2.push(start); //put letter at the end
+
+    //check word in list of words
+    var isaword = false;
+    for (var k = 0; k < words.length; k++) {
+      break;
+    }
+  }
+}
