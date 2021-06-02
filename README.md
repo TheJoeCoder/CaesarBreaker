dfc00d77a7e8e5801db6d59f453e8c51a56f8223
TheJoeCoder <joe@radialbog9.uk>
2021-06-02 21:55:56 +0100

    Huge update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CaesarBreaker
 Really simple caesar cipher breaker
+
+## Usage
+Replace `ifmmp uifsf` with the text you want to try to decrypt, or run it with the preset message.
+```
+git clone https://github.com/TheJoeCoder/CaesarBreaker && cd CaesarBreaker
+node index.js ifmmp uifsf
+```
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-// Caesar cipher breaker
-const fs = require("fs");
+#!/usr/bin/env node
 
-const list = fs.readFileSync("./words.txt").toString();
-const words = list.split("\n");
+/*  
+  ____                          ____                 _
+ / ___|__ _ ___  ___  __ _ _ __| __ ) _ __ ___  __ _| | _____ _ __
+| |   / _` / __|/ _ \/ _` | '__|  _ \| '__/ _ \/ _` | |/ / _ \ '__|
+| |__| (_| \__ \  __/ (_| | |  | |_) | | |  __/ (_| |   <  __/ |
+ \____\__,_|___/\___|\__,_|_|  |____/|_|  \___|\__,_|_|\_\___|_|
 
-const args = process.argv.slice(2);
+  Code to break a caesar cipher using a list of words.
+  License: GPL3 (basically use it how you like; for more details read the LICENSE file)
+*/
+
+const fs = require("fs"); //Load the filesystem library so we can write to files
+
+const list = fs.readFileSync("./words.txt").toString(); //Load the word list
+const words = list.split("\n"); //Split the word list by new lines (\n)
+
+const args = process.argv.slice(2); //get command line arguments
 
 var letters1 = [
   "A",
@@ -35,27 +47,51 @@ var letters1 = [
   "Z"
 ];
 
-var letters2 = letters1;
+var letters2 = letters1; //Creates cipher list/letters2 with the contents of letters1
 
-function decipher(text) {
+
+/*
+  This function deciphers the input text with the current contents of 
+  `letters1` and `letters2`. Letters2 is the list with the cipher rotated
+  (e.g A in letters1 is B in cipher code/letters2).
+  What this code does is look for each letter of the word in letters2 and 
+  then look up the letter in the same position, but in letters1 instead.
+  This is what I used to cipher the savegames in my Scratch savegame sys-
+  tem (https://scratch.mit.edu/projects/523818864/)
+*/
+function decipher(text = "") {
+  var cipheredstring = "";
   for(var i = 0; i < text.length; i++) {
-    //TODO: decipher code
+    var letter = text.charAt(i);
+    /*
+      Find ciphered letter from table 2 (rotated) and get letter in same position in 
+      table 1 (not rotated), therefore deciphering with the current cipher positions.
+      We are also checking if the character provided is not in the table (e.g anything 
+      that is not a letter) and we ignore it (don't search for it in the other list) if so.
+    */
+    //var cipheredletter = letter.toUpperCase();
+    //var index = letters2.indexOf(letter.toUpperCase());
+    //if(index != -1) cipheredletter = letters1[index];
+    var cipheredletter = letters1[letters2.indexOf(letter.toUpperCase())];
+    
+    cipheredstring += cipheredletter;
   }
+  return cipheredstring;
 }
 
-//loop through all words specified in the arguments
+//loop through all words specified
 for (var i = 0; i < args.length; i++) {
   //loop through all cipher patterns
   for (var j = 1; j < letters1.length; j++) {
     //cycle cipher
-    console.log(`Using cipher ${j} (A = ${letters2[0]})`);
-    var start = letters2[0]; //store first letter
-    letters2.shift(); //remove first letter
-    letters2.push(start); //put letter at the end
+    letters2.push(letters2.shift());
+    //console.log(`Using cipher ${j} (A = ${letters2[0]})`);
 
     //check word in list of words
-    if(words.includes(decipher(letters1))) {
-      //TODO: log the match
+    var dc = decipher(args[i]);
+    console.log(dc);
+    if(words.includes(dc)) {
+      console.log(`Word match: ${args[i]} -> ${dc} (Cipher ${j})`);
     }
   }
 }
