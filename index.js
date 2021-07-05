cc67ce17e76b94229f2630787edb7e48b23f5424
TheJoeCoder <joe@radialbog9.uk>
2021-07-05 15:26:37 +0100

    Fix indentation

--- index.js
+++ index.js
@@ -18,72 +18,38 @@ const words = list.split("\n"); //Split the word list by line breaks (usually \n
 
 //loop through all words and make them uppercase (but not in the words list file)
 for(var i = 0; i < words.length; i++) {
-  words[i] = words[i].toUpperCase();
+    words[i] = words[i].toUpperCase();
 }
 
 const args = process.argv.slice(2); //get command line arguments apart from the first 2 (which are probably `node index.js`)
 
 var letters1 = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
+    "A", "B","C", "D", "E", "F",
+    "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R",
+    "S", "T", "U", "V", "W", "X",
+    "Y", "Z"
 ];
 
 var letters2 = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
+    "A", "B","C", "D", "E", "F",
+    "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R",
+    "S", "T", "U", "V", "W", "X",
+    "Y", "Z"
 ];
 
-var wordmatches = {
+function resetCipher() {
+    letters2 = [
+        "A", "B","C", "D", "E", "F",
+        "G", "H", "I", "J", "K", "L",
+        "M", "N", "O", "P", "Q", "R",
+        "S", "T", "U", "V", "W", "X",
+        "Y", "Z"
+    ];
+}
 
-};
+var wordmatches = {};
 
 /*
   This function deciphers the input text with the current contents of 
@@ -95,45 +61,38 @@ var wordmatches = {
   tem (https://scratch.mit.edu/projects/523818864/)
 */
 function decipher(text = "") {
-  var cipheredstring = "";
-  for(var i = 0; i < text.length; i++) {
-    var letter = text.charAt(i).toUpperCase();
-    /*
-      Find ciphered letter from table 2 (rotated) and get letter in same position in 
-      table 1 (not rotated), therefore deciphering with the current cipher positions.
-      We are also checking if the character provided is not in the table (e.g anything 
-      that is not a letter) and we ignore it (don't search for it in the other list) if so.
-    */
-    var cipheredletter = letter;
-    var index = letters2.indexOf(letter);
-    if(index != -1) cipheredletter = letters1[index];
-    
-    cipheredstring += cipheredletter;
-  }
-  return cipheredstring;
+    var cipheredstring = "";
+    for(var i = 0; i < text.length; i++) {
+        var letter = text.charAt(i).toUpperCase();
+        /*
+            Find ciphered letter from table 2 (rotated) and get letter in same position in 
+            table 1 (not rotated), therefore deciphering with the current cipher positions.
+            We are also checking if the character provided is not in the table (e.g anything 
+            that is not a letter) and we ignore it (don't search for it in the other list) if so.
+        */
+        var cipheredletter = letter;
+        var index = letters2.indexOf(letter);
+        if(index != -1) cipheredletter = letters1[index];
+        cipheredstring += cipheredletter;
+    }
+    return cipheredstring;
 }
 
 //loop through all words specified
 for (var i = 0; i < args.length; i++) {
-  //loop through all cipher patterns
-  for (var j = 1; j < letters1.length; j++) {
-    //cycle cipher
-    letters2.push(letters2.shift());
-    //check word in list of words
-    var dc = decipher(args[i]);
-    if(words.includes(dc.toUpperCase())) {
-      console.log(`Word match: ${args[i].toUpperCase()} -> ${dc} (Cipher ${j})`);
-      wordmatches[j] = wordmatches[j] == null ? 1 : wordmatches[j] + 1; //add 1 to match count for ciphers
+    //loop through all cipher patterns
+    for (var j = 1; j < letters1.length; j++) {
+        //cycle cipher
+        letters2.push(letters2.shift());
+        //check word in list of words
+        var dc = decipher(args[i]);
+        if(words.includes(dc.toUpperCase())) {
+            console.log(`Word match: ${args[i].toUpperCase()} -> ${dc} (Cipher ${j})`);
+            wordmatches[j] = wordmatches[j] == null ? 1 : wordmatches[j] + 1; //add 1 to match count for ciphers
+        }
     }
-  }
-  //reset cipher after each word
-  letters2 = [
-    "A", "B","C", "D", "E", "F",
-    "G", "H", "I", "J", "K", "L",
-    "M", "N", "O", "P", "Q", "R",
-    "S", "T", "U", "V", "W", "X",
-    "Y", "Z"
-  ];
+    //reset cipher after each word
+    resetCipher();
 }
 
 console.log("FINISHED!");
