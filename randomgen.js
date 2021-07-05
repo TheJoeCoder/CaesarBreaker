813dabc933a4bfbe62f01c1f58decb255e3282b8
TheJoeCoder <joe@radialbog9.uk>
2021-07-05 23:08:40 +0100

    Fix grammar and finish random generator

--- randomgen.js
+++ randomgen.js
@@ -7,11 +7,12 @@
 | |__| (_| \__ \  __/ (_| | |  | |_) | | |  __/ (_| |   <  __/ |
  \____\__,_|___/\___|\__,_|_|  |____/|_|  \___|\__,_|_|\_\___|_|
 
-  Code to break a caesar cipher using a list of English words.
+  Code to break a caesar cipher using a list of words.
   License: GPL3 (basically use it how you like; for more details read the LICENSE file)
 */
 
-const fs = require("fs"); //Load the filesystem library so we can read from and write to files
+const removeDiacritics = require("diacritics").remove; //load the diacritics module to remove the diacritics
+const fs = require("fs"); //Load the filesystem module inbuilt into nodejs so we can read from and write to files
 
 //Cipher setup
 var letters1 = [
@@ -39,6 +40,33 @@ function resetCipher() {
     ];
 }
 
+/*
+  This function ciphers the input text with the current contents of 
+  `letters1` and `letters2`. Letters2 is the list with the cipher rotated
+  (e.g A in letters1 is B in cipher code/letters2).
+  What this code does is look for each letter of the word in letters1 and 
+  then look up the letter in the same position, but in letters2 instead, 
+  this being the opposite of the decipher function in index.js. This is 
+  what I used to cipher the savegames in my Scratch savegame system 
+  (https://scratch.mit.edu/projects/523818864/).
+*/
+function cipher(text = "") {
+    var cipheredstring = "";
+    for(var i = 0; i < text.length; i++) {
+        var letter = removeDiacritics(text.charAt(i)).toUpperCase();
+        /*
+            Find ciphered letter from table 1 (not rotated) and get letter in same position in 
+            table 2 (rotated), therefore ciphering with the current cipher positions.
+            We are also checking if the character provided is not in the table (e.g anything 
+            that is not a letter) and we ignore it (don't search for it in the other list) if so.
+        */
+        var cipheredletter = letter;
+        var index = letters1.indexOf(letter);
+        if(index != -1) cipheredletter = letters2[index];
+        cipheredstring += cipheredletter;
+    }
+    return cipheredstring;
+}
 
 //random between 2 integers
 function randInt(min, max) {
@@ -51,15 +79,31 @@ function randInt(min, max) {
 const list = fs.readFileSync("./words.txt").toString(); //Load the word list
 const words = list.split("\n"); //Split the word list by line breaks (usually \n or \r\n, but in this case \n because file is encoded with LF line endings, not CRLF endings)
 
-//loop through all words and make them uppercase (but not in the words list file)
+//remove diacritics (è, å, ï, etc) from every word and make them uppercase (but not in the words list file)
 for(var i = 0; i < words.length; i++) {
-  words[i] = words[i].toUpperCase();
+    words[i] = removeDiacritics(words[i]).toUpperCase();
 }
 
-//generate 5 phrases (0<=x<6)
+//generate 5 phrases (0 <= i < 6)
 for(var i = 0; i < 6; i++) {
     //how many words (between 1 and 3)
     var length = randInt(1, 3);
     //how much to cipher (between 1 and 25, 26 would be a full rotation)
-    var cipher = randInt(1, 25);
+    var ciphert = randInt(1, 25);
+    //pick random words
+    var randwords = "";
+    for(var j = 0; j < length; j++) {
+        randwords += `${words[randInt(0, words.length - 1)]} `;
+    }
+    //remove space from end of random list
+    randwords = randwords.trim();
+    //rotate cipher number of times
+    for(var j = 0; j < ciphert; i++) {
+        letters2.push(letters2.shift());
+    }
+    //cipher words
+    var ciphered = cipher(randwords);
+    console.log(`${randwords} -> cipher ${ciphert} -> ${ciphered}`);
+    //reset cipher
+    resetCipher();
 }
